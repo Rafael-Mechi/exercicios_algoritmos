9de13f2ce701bce2e94ca06268b6a4bba360612c
Rafael-Mechi <rafael.doliveira@sptech.school>
2025-05-15 13:17:35 -0300

    Tentativa do desafio da aula 20

--- aula_19/lista_14/par_ou_impar.js
+++
@@ -1,9 +0,0 @@
-function ehParOuImpar(numero){
-    if(numero % 2 == 0){
-        return "par";
-    }
-    return "ímpar";
-}
-
-console.log(ehParOuImpar(8));
-console.log(ehParOuImpar(23));
--- aula_19/lista_14/soma.js
+++
@@ -1,9 +0,0 @@
-function somar(a, b){
-    soma = a + b;
-    return soma;
-}
-
-console.log(somar(2, 2));
-console.log(somar(12, 3));
-console.log(somar(100, 200));
-
