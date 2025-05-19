d384894a536024995de758d2b01541285fa3a185
Rafael-Mechi <rafael.doliveira@sptech.school>
2025-05-19 15:13:19 -0300

    desafio do cinema adicionado. CONTINUAR

---
+++ aula_21/dados.js
@@ -0,0 +1,38 @@
+var pacientes = [
+    {
+        nome: "Bob",
+        sobrenome: "Silva e Silva",
+        sintomas: ["Febre", "Dor de Cabeça", "Tontura"],
+        codigoAtendimento: "COD001"
+    },
+    {
+        nome: "Tom",
+        sobrenome: "Souza",
+        sintomas: ["Dor no peito", "Náusea"],
+        codigoAtendimento: "COD002"
+    },
+    {
+        nome: "Leo",
+        sobrenome: "Costa",
+        sintomas: ["Fadiga", "Tosse"],
+        codigoAtendimento: "COD003"
+    },
+    {
+        nome: "Ana",
+        sobrenome: "Dias",
+        sintomas: ["Dificuldade para respirar", "Falta de apetite"],
+        codigoAtendimento: "COD004"
+    },
+    {
+        nome: "Eli",
+        sobrenome: "Moura",
+        sintomas: ["Dor abdominal", "Vômito"],
+        codigoAtendimento: "COD005"
+    },
+    {
+        nome: "Max",
+        sobrenome: "Ferreira",
+        sintomas: ["Insônia", "Ansiedade"],
+        codigoAtendimento: "COD006"
+    }
+];  
